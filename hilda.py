8370a42791caecfd2250d00b7fed67b7d7978a17
John Evans <john@milo.com>
2009-09-22 21:27:32 -0700

    Initial commit.

---
+++ hilda.py
@@ -0,0 +1,40 @@
+from functools import wraps
+from collections import defaultdict
+
+
+def memoize(f):
+    @wraps(f)
+    def wrapped(self, *args, **kwargs):
+        if not hasattr(self, "_memo_cache"):
+            self._memo_cache = defaultdict(dict)
+        memo_cache = self._memo_cache
+        method_cache = self._memo_cache[f.__name__]
+        key = (args, repr(kwargs))
+        if key not in method_cache:
+            method_cache[key] = apply(f, [self] + list(args), kwargs)
+        return method_cache[key]
+    return wrapped
+
+
+class Table(object):
+    def __init__(self, name):
+        self.name = name
+
+
+class Database(object):
+    def __init__(self, driver):
+        self.driver = driver
+
+    @memoize
+    def tables(self):
+        cursor = self.driver.cursor()
+        rows = cursor.execute("""
+            SELECT name FROM sqlite_master
+            WHERE type='table'
+            ORDER BY name;
+        """).fetchall()
+        return [Table(row[0]) for row in rows]
+
+    def forget(self):
+        if hasattr(self, "_memo_cache"):
+            del self.__dict__["_memo_cache"]
---
+++ tests/basic.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+import unittest
+import sqlite3
+
+from hilda import Database
+
+# TODO: Dialects
+
+
+class DatabaseTests(unittest.TestCase):
+    def setUp(self):
+        self.db1 = sqlite3.connect(":memory:")
+        cursor = self.db1.cursor()
+        cursor.execute("""CREATE TABLE foo (
+                              id INTEGER PRIMARY KEY,
+                              value VARCHAR(255)
+                          );""")
+
+        cursor.execute("""CREATE TABLE bar (
+                              id INTEGER PRIMARY KEY,
+                              value VARCHAR(255) NOT NULL
+                          );""")
+
+        self.db2 = sqlite3.connect(":memory:")
+        cursor = self.db2.cursor()
+        cursor.execute("CREATE TABLE baz (bif INTEGER PRIMARY KEY)")
+
+    def test_can_list_tables(self):
+        database = Database(self.db1)
+        tables = database.tables()
+        self.assertEqual(2, len(tables))
+        expected_table_names = ("foo" ,"bar")
+        for table in tables:
+            self.assert_(bool(table))
+            self.assert_(table.name in expected_table_names)
+
+    def test_listed_tables_are_cached(self):
+        database = Database(self.db2)
+        table1 = database.tables()[0]
+        table2 = database.tables()[0]
+        self.assert_(table1 == table2)
+
+    def test_listed_tables_are_not_cached_after_forget(self):
+        database = Database(self.db2)
+        table1 = database.tables()[0]
+        database.forget()
+        table2 = database.tables()[0]
+        self.assert_(table1 != table2)
+
+    def tearDown(self):
+        self.db1.close()
+        self.db2.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
