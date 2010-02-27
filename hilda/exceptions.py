838ec1736cf57432036587eeb881e6b946e1887a
John Evans <john@milo.com>
2010-02-26 16:22:10 -0800

    Added docstring to TooManyResultsFound exception.

--- hilda/exceptions.py
+++ hilda/exceptions.py
@@ -6,8 +6,11 @@ class HildaException(Exception):
 
 class NoResultFound(HildaException):
     """Raised when a method expecting a result does not find one."""
+
     pass
 
 
 class TooManyResultsFound(HildaException):
+    """Raised when a method gets more results than it expected."""
+
     pass
